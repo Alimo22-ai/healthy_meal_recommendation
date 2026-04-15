fc5e3dd4a100602eeec0d4e5acadf04452a25766
Ali Mohamed <01157572404a@gmail.com>
2026-04-15 14:35:20 +0200

    Delete 15-4-2026 -NEW directory

--- 15-4-2026 -NEW/train_model.py
+++
@@ -1,8 +0,0 @@
-from meal_model import UserNutritionModel
-
-
-if __name__ == "__main__":
-    model = UserNutritionModel()
-    model.train("nutrition_dataset.csv")
-    model.save("nutrition_model.joblib")
-    print("Saved model to nutrition_model.joblib")
